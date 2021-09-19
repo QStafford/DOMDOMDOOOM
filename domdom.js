5e1cd472765ae1f83335e2bd0f0cc00e37f76711
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-19 18:06:31 -0500

    added id and when clicked is incremented

--- domdom.js
+++ domdom.js
@@ -1,20 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
     let button = document.createElement("button");
     let btnText = document.createTextNode("Add Square");
-
+    let i = 0
     button.appendChild(btnText)
 
     document.body.appendChild(button)
 
     button.addEventListener("click", function(){
         let Square = document.createElement("div")
-        Square.className = 'BlackBox' 
+       
+        Square.className = 'BlackBox'
+        
+        Square.id = i++ 
+        
+        
+        
         Square.style.width = "50px"
         Square.style.height = "50px"
         Square.style.border = "30px solid black"
         Square.style.padding = "50px solid black"
         Square.style.margin = "20px"
         Square.style.float = 'right'
+       
+       
         document.body.appendChild(Square)
+        
+        
     })
 })
